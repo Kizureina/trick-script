9be880984700231c31af6b516df54ad4433896c7
Kizureina <houchangkun@gmail.com>
2024-02-29 14:07:42 +0800

    优化文件下载

--- kczx-download-file-all-in-one.js
+++ kczx-download-file-all-in-one.js
@@ -1,29 +1,26 @@
-function getUrl(contentId, callback) {
-    var xhr = new XMLHttpRequest();
-    var url = 'https://kczx.cuit.edu.cn/learn/v1/course/file/info';
-    var params = [];
-    params.push(contentId);
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var responseData = JSON.parse(xhr.responseText);
-                console.log(responseData.data[0].filePath + "获取成功");
-                var result = "https://kczx.cuit.edu.cn" + responseData.data[0].filePath;
-                callback(result); // 调用回调函数并传递结果
-            } else {
-                console.error('Error occurred: ' + xhr.status);
-            }
-        }
-    };
-    xhr.send(JSON.stringify(params));
-}
+// treeInfo[0].childInfo[0].info.chapter_describe
 
-// 使用示例
-// getUrl(contentId, function (result) {
-//     console.log("获取到的字符串为：" + result);
-// });
+function getUrl(contentId) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        var url = 'https://kczx.cuit.edu.cn/learn/v1/course/file/info';
+        var params = [contentId];
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var responseData = JSON.parse(xhr.responseText);
+                    var result = "https://kczx.cuit.edu.cn" + responseData.data[0].filePath;
+                    resolve(result);
+                } else {
+                    reject('Error occurred: ' + xhr.status);
+                }
+            }
+        };
+        xhr.send(JSON.stringify(params));
+    });
+}
 
 function downloadFile(fileUrl) {
     // 使用正则表达式匹配最后一个斜杠后的内容
@@ -52,23 +49,74 @@ function downloadFile(fileUrl) {
 
     xhr.send();
 }
-// treeInfo[0].childInfo[0].info.chapter_describe
 
-const fileDownloadUrl = "";
 
-for (let index = 0; index < treeInfo.length; index++) {
-    const childInfo = treeInfo[index].childInfo;
-    for (let j = 0; j < childInfo.length; j++) {
-        const chapter_describe = childInfo[j].info.chapter_describe;
-        console.log(chapter_describe);
-        getUrl(chapter_describe, function (result) {
-            console.log("获取到的字符串为：" + result);
-            fileDownloadUrl = result;
-    
-            // 下载文件
-            downloadFile(fileDownloadUrl);
+// 使用递归实现批量下载
+function downloadFiles(fileUrls) {
+    if (fileUrls.length === 0) {
+        return; // 结束递归
+    }
+
+    const fileUrl = fileUrls.shift(); // 取出第一个文件链接
+    getUrl(fileUrl)
+        .then((result) => {
+            console.log("成功获取文件链接：" + result);
+            downloadFile(result); // 下载文件
+            downloadFiles(fileUrls); // 递归调用，处理下一个文件链接
+        })
+        .catch((error) => {
+            console.error("获取文件链接失败：" + error);
+            downloadFiles(fileUrls); // 继续处理下一个文件链接
         });
+}
+
+// for (let index = 0; index < treeInfo.length; index++) {
+//     const childInfo = treeInfo[index].childInfo;
+
+//     for (let j = 0; j < childInfo.length; j++) {
+//         let resourse_type = childInfo[j].info.resourse_type;
+//         if (resourse_type == "document") {
+//             const chapter_describe = childInfo[j].info.chapter_describe;
+//             console.log(chapter_describe);
+//             fileDownloadUrls.push(chapter_describe);
+//         }
+
+//     }
+// }
+
+// downloadFiles(fileDownloadUrls);
+
+const fileDownloadUrls = [];
+
+async function processDownloads() {
+    for (let index = 0; index < treeInfo.length; index++) {
+        const childInfo = treeInfo[index].childInfo;
+
+        for (let j = 0; j < childInfo.length; j++) {
+            let resourse_type = childInfo[j].info.resourse_type;
+            if (resourse_type === "document" || resourse_type === "other") {
+                const chapter_describe = childInfo[j].info.chapter_describe;
+                console.log(chapter_describe);
+                try {
+                    const result = await getUrl(chapter_describe);
+                    console.log("获取到的字符串为：" + result);
+                    fileDownloadUrls.push(result);
+                } catch (error) {
+                    console.error("获取文件链接失败：" + error);
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < fileDownloadUrls.length; i++) {
+        try {
+            await downloadFile(fileDownloadUrls[i]);
+            console.log("文件下载成功");
+        } catch (error) {
+            console.error("文件下载失败：" + error);
+        }
     }
 }
 
+processDownloads();
 
